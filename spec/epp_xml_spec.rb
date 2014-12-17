4b5486ef0c94c3d03328521ea7f1890009a5cb28
Martin Lensment <mlensment@gmail.com>
2014-12-17 16:51:58 +0200

    Add clTRID spec

---
+++ spec/epp_xml_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe EppXml do
+  it 'sets correct clTRID' do
+    ex = EppXml::Domain.new(cl_trid: 'asd')
+    expect(ex.clTRID).to eq('asd')
+    ex.cl_trid = nil
+    expect(ex.clTRID).to be_within(2).of(Time.now.to_i)
+    ex.cl_trid_prefix = 'bla'
+    expect(ex.clTRID.split('-')[0]).to eq('bla')
+    expect(ex.clTRID.split('-')[1].to_i).to be_within(2).of(Time.now.to_i)
+  end
+end
